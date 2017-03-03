1bebcb6f37d14a9897509111d4343a37c0e5188c
anwangwang <wangwang@tc2025.com>
2017-03-03 17:49:14 +0800

    1.代码实现

---
+++ app/src/main/java/com/awwhome/xiezaianzhuangapp/receiver/AppStateReceiver.java
@@ -0,0 +1,30 @@
+package com.awwhome.xiezaianzhuangapp.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by Administrator on 2017/3/3 0003.
+ */
+public class AppStateReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "AppStateReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "onReceive: onReceive方法被执行了");
+
+        String action = intent.getAction();
+
+        if ("android.intent.action.PACKAGE_ADDED".equals(action)){
+            Log.d(TAG, "onReceive: 应用被安装了 -----ADDED");
+        }else if ("android.intent.action.PACKAGE_INSTALL".equals(action)){
+            Log.d(TAG, "onReceive: 应用被安装了 -----INSTALL");
+        }else if ("android.intent.action.PACKAGE_REMOVED".equals(action)){
+            Log.d(TAG, "onReceive: 应用被卸载了 ");
+        }
+
+    }
+}
